d6a9534dd6fbb2aceaf0e1d8b40223a08bb9cd86
sergii-onyx <twilight.cympak@gmail.com>
2022-06-20 20:27:42 -0400

    created about page and navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
