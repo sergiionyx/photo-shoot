0b26965774f184b357be9dced67328821f437605
sergii-onyx <twilight.cympak@gmail.com>
2022-06-22 16:16:00 -0400

    added photo rendering based on category

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
-
 
 function App() {
   const [categories] = useState([
@@ -30,7 +29,7 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
