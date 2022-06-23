1a76b17ffe6b0868e1a76a597810601aea7b4001
sergii-onyx <twilight.cympak@gmail.com>
2022-06-23 10:12:53 -0400

    added contact rendering and styling based on active nav el. added tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,19 +1,19 @@
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", () => {
-  // First Test
-  it("renders", () => {
+describe('About component renders', () => {
+  it('renders', () => {
     render(<About />);
   });
-
-  // Second Test
+  
   it('matches snapshot DOM node structure', () => {
-    // render About
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,50 +1,37 @@
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
-
-const categories = [
-  { name: "portraits", description: "Portraits of people in my life" },
-];
-const mockCurrentCategory = jest.fn();
-const mockSetCurrentCategory = jest.fn();
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
-  // baseline test
-  it("renders", () => {
-    render(
-      <Nav
-        categories={categories}
-        setCurrentCategory={mockSetCurrentCategory}
-        currentCategory={mockCurrentCategory}
-      />
-    );
+describe('Nav component renders', () => {
+  it('renders', () => {
+    render(<Nav />);
   });
 
-  //snapshot test
-  it("matches snapshot", () => {
+  it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-
-    // assert value comparison
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
 
-describe("emoji is visible", () => {
-  it("inserts emoji into the h2", () => {
-    const { getByLabelText } = render(<Nav />);
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+  const { getByLabelText } = render(<Nav />);
 
-    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-});
+})  
 
-describe("links are visible", () => {
-  it("inserts text into the links", () => {
-    // Arrange
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-    // Assert
-    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-    expect(getByTestId("about")).toHaveTextContent("About me");
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
   });
-});
+
+})
